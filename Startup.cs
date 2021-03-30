fb37779158a536cf272fd2b61fcca7d33abd9897
Stachozaur <stachowicz.karol@gmail.com>
2021-03-30 20:30:37 +0200

    Updated Database, Migrations

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using Job.it_classes.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,12 @@ namespace Job.it_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<BaseDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("JobItDB"));
+            }
+            );
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
