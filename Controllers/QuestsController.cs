5dec8ca7f4d4e7f46dafb88c8bef37636a6ded64
Stachozaur <stachowicz.karol@gmail.com>
2021-03-31 12:07:10 +0200

    Controller update

---
+++ Controllers/QuestsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job.it_API.Controllers
+{
+    [Route("api/[controller]")]
+    public class QuestsController : ControllerBase
+    {
+        public QuestsController()
+        {
+
+        }
+
+        public object Get()
+        {
+            return new { QuestId = 5, Name = "BlaBla" };
+        }
+    }
+}
